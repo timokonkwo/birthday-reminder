3403f73208f82e3b19c0324b7094b21a94e238aa
Tim <chibuiketimokonkwo@gmail.com>
2022-10-20 12:29:46 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return (
-    <div className='section'>
-        <main className='main'>
-          <div className='container'>
-            <h3>24 birthdays today</h3>
-              <div className='person'>
-                <img src='https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg'/>
-                <h4>Bertie Yates</h4>
-                <p>29 years</p>
-              </div>
-              <button>View all</button>
-          </div>
-      </main>
-    </div>
-  )
 
+  const [people, setPeople] = useState(data)
+
+  return <main>
+      <section className='container'>
+        <h3>{people.length} birthdays</h3>
+        <List people = {people}/>
+        <button onClick={() => console.log("Clicked")}>Clear All</button>
+      </section>
+  </main>
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const List = () => {
+const List = ({people}) => {
   return (
     <>
-      <img src='https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg'/>
+       <ul>
+        {people}
+        </ul>
     </>
   );
 };
